745af5d14e1414ff5a8096e2d08040b9babe1db1
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-18 17:03:19 +0300

    commit tree b1dcb94db12a738125c51a1b85361ebd2d52d433

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length, i !== j; j++) {
+    if (array[i] + array[j] === target) {
+      return true;
+      }
+    }
+  }
+  return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
